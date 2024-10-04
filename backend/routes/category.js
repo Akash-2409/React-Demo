a86bb27b910d15d6be7a6c8ebc47f88b427ec735
Akash-2409 <akashboda24@gmail.com>
2024-10-04 13:12:21 +0530

    category add-update-delete

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -25,4 +25,57 @@ router.post("/add-category",authenticateToken,async (req,res) =>{
     }
 });
 
+// Display Category
+router.get("/category",async (req,res)=>{
+    try {
+        
+        const cate = await Category.find({});
+        // if (!cate) {
+        //     return res.status(400).json({message:"Category Not Found"});
+        // }
+        res.status(200).json(cate);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+});
+
+// Category By ID
+router.get("/getcategory",async (req,res)=>{
+    try {
+        const {id} = req.headers;
+        const cate = await Category.findById(id);
+        if (!cate) {
+            return res.status(400).json({message:"Category Not Found"});
+        }
+        res.status(200).json(cate);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+});
+
+// Update Category
+router.put("/updateCategory",authenticateToken,async (req,res)=>{
+    try {
+        const {id} = req.headers;
+        const {name} = req.body;
+        const cate = await Category.findByIdAndUpdate(id,{name:name});
+        res.status(200).json("Category Changed Successfully....")
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+});
+
+router.delete("/deleteCategory",authenticateToken,async (req,res)=>{
+    try {
+        const {id} = req.headers;
+        const cate = await Category.findByIdAndDelete(id);
+        if (authenticateToken == null) {
+            return res.status(403).json("You Are Not Admin");
+        }
+        res.status(200).json("Category Deleted Successfully....")
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
 module.exports = router;
